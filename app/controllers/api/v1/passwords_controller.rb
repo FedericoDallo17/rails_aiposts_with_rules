fdbbcf2fe7832ee30c178dc021786229466f9c42
federicodallo17 <federico@neocoast.com>
2025-11-04 18:28:23 -0300

    fix: Configure Devise to accept additional parameters for signup
    - Added configure_permitted_parameters to ApplicationController
    - Updated RegistrationsController with sign_up_params and account_update_params
    - Added skip_before_action :verify_authenticity_token to all Devise controllers
    - Now properly accepts username and other custom fields during registration
    - Fixes 'Email can't be blank' error even when parameters are sent correctly

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class PasswordsController < Devise::PasswordsController
       respond_to :json
+      skip_before_action :verify_authenticity_token
 
       def create
         self.resource = resource_class.send_reset_password_instructions(resource_params)
--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class RegistrationsController < Devise::RegistrationsController
       respond_to :json
+      skip_before_action :verify_authenticity_token
 
       private
 
@@ -16,6 +17,14 @@ module Api
             status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
           }, status: :unprocessable_entity
         end
+      end
+
+      def sign_up_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :username, :first_name, :last_name, :bio, :website, :location)
+      end
+
+      def account_update_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :username, :first_name, :last_name, :bio, :website, :location, :profile_picture, :cover_picture)
       end
     end
   end
